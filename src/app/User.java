8c5a37b7d6555391818000b69c0dc536fd6d682d
a5ms <168677037+a5ms@users.noreply.github.com>
2025-05-12 16:34:34 +0300

    Update User.java

--- src/app/User.java
+++ src/app/User.java
@@ -1,11 +1,24 @@
 package app;
+
 public abstract class User {
     protected String name;
     protected int age;
+    protected String username;
+    protected String password;
 
-    public User(String name, int age) {
+    public User(String name, int age, String username, String password) {
         this.name = name;
         this.age = age;
+        this.username = username;
+        this.password = password;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public boolean checkPassword(String inputPassword) {
+        return this.password.equals(inputPassword);
     }
 
     public abstract String getInfo();
