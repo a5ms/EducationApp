d51185220b41266c7018dda35e7906e7349d80f1
a5ms <168677037+a5ms@users.noreply.github.com>
2025-05-12 16:32:42 +0300

    Update Teacher.java

--- src/app/Teacher.java
+++ src/app/Teacher.java
@@ -1,10 +1,27 @@
 package app;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class Teacher extends User {
     private String subject;
+    private List<Course> courses = new ArrayList<>();
 
-    public Teacher(String name, int age, String subject) {
-        super(name, age);
+    public Teacher(String name, int age, String subject, String username, String password) {
+        super(name, age, username, password);
         this.subject = subject;
+    }
+
+    public void addCourse(Course course) {
+        courses.add(course);
+    }
+
+    public List<Course> getCourses() {
+        return courses;
+    }
+
+    public String getSubject() {
+        return subject;
     }
 
     @Override
@@ -12,4 +29,3 @@ public class Teacher extends User {
         return "Teacher - Name: " + name + ", Age: " + age + ", Subject: " + subject;
     }
 }
-    
